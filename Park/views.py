14f122c37104d4534e9f6364b378bd02ac64ee04
Maximo DeLaRosa <mjd809@gmail.com>
2018-10-08 19:36:17 -0400

    Basic python structure for backend

---
+++ Park/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index():
+  def dispatch(request, *args, **kwargs):
+        response_text = textwrap.dedent('''\
+            <html>
+            <head>
+                <title>Greetings to the world</title>
+            </head>
+            <body>
+                <h1>Greetings to the world</h1>
+                <p>Hello, world!</p>
+            </body>
+            </html>
+        ''')
+        return HttpResponse(response_text)
+
+
