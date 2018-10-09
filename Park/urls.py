af9a03d2a2f00b827bb2f5c0a8b0f69589e65964
Maximo DeLaRosa <mjd809@gmail.com>
2018-10-09 14:03:30 -0400

    Practice models
    Full functionality not written yet

--- Park/urls.py
+++ Park/urls.py
@@ -4,5 +4,10 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-  path('', views.index),
+  path('<question_id>', views.detail),
+  path('<int:question_id>/', views.detail, name='detail'),
+  # ex: /polls/5/results/
+  path('<int:question_id>/results/', views.results, name='results'),
+  # ex: /polls/5/vote/
+  path('<question_id>/vote/', views.vote, name='vote'),
 ]
