6a2d465b982c70211421472d2df17512a5d35082
Maximo DeLaRosa <mjd809@gmail.com>
2018-10-21 14:50:59 -0400

    Added ability to search for adresses-mStill need to add functionality to query location to google api

--- client/screens/Searchbox.js
+++ client/screens/Searchbox.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Text,TextInput, KeyBoard } from 'react-native';
 import { View, InputGroup, Input } from 'native-base';
 import { FontAwesome } from "react-native-vector-icons";
 import { Dropdown } from 'react-native-material-dropdown';
 import styles from './SearchStyles';
 
+class SearchBox extends Component {
+  state = { 
+    location: ''
+   }
 
-const SearchBox = (props) => {
-  return ( 
-    <View style={styles.searchBox}>
-      {!props.searching ?
-        <View style={styles.inputWrapper}>
-          <Text style={styles.label}>Leaving parking spot</Text>
-          <InputGroup>
-            <FontAwesome name='search' size={15} color='#FF5E3A'/>
-            <Input style={styles.inputSearch} placeholder='Enter your current location' />
-          </InputGroup>
-        </View>
-      : <View style={styles.inputWrapper}>
-          <Text style={styles.label}>Searching for parking</Text>
-          <InputGroup>
-            <FontAwesome name='search' size={15} color='#FF5E3A'/>
-            <Input  style={styles.inputSearch} placeholder='Choose parking location' />
-          </InputGroup>
-        </View>
-      }
-    </View>   
-  );
+  componentWillMount () {
+    // this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this._keyboardDidShow);
+    // this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
+  }
+
+
+  locateQuery = () => {
+    let url = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+    let address = url + this.state.location + process.env.MAP_KEY;
+    console.log(address);
+  }
+
+  render() {
+    console.log(this.state.location); 
+    return ( 
+      <View style={styles.searchBox}>
+        {!this.props.searching ?
+          <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Leaving parking spot</Text>
+            <InputGroup>
+              <FontAwesome name='search' size={15} color='#FF5E3A'/>
+              <Input 
+                style={styles.inputSearch} 
+                value={this.state.location}
+                name='location'
+                onChangeText={(location) => this.setState({ location })} 
+                placeholder='Enter your current location'
+                returnKeyType="search"
+                />
+            </InputGroup>
+          </View>
+        : <View style={styles.inputWrapper}>
+            <Text style={styles.label}>Searching for parking</Text>
+            <InputGroup>
+              <FontAwesome name='search' size={15} color='#FF5E3A'/>
+              <Input 
+                style={styles.inputSearch} 
+                value={this.state.location}
+                name='location'
+                onChangeText={(location) => this.setState({ location })} 
+                placeholder='Choose parking location'
+                returnKeyType="search"
+                />
+            </InputGroup>
+          </View>
+        }
+      </View>
+    )   
+  }
 }
-
+ 
 export default SearchBox;
