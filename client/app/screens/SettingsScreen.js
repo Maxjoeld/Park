26148880fec2567bc1fb2b474ecb212b7c59aec7
Maximo DeLaRosa <mjd809@gmail.com>
2018-10-27 20:38:16 -0400

    Added inputs to settings.js

--- client/app/screens/SettingsScreen.js
+++ client/app/screens/SettingsScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ScrollView, Text, StyleSheet, View } from 'react-native';
 import { Header,Left,Right,Icon } from 'native-base';
+import { View, InputGroup, Input } from 'native-base';
+
 import { FontAwesome } from "react-native-vector-icons";
 import Mod from "react-native-modal";
 
@@ -20,6 +22,31 @@ export default class SettingsScreen extends React.Component {
             <Icon style={{ justifyContent: 'center'}} name="menu" onPress={() => this.props.navigation.openDrawer()}/>
           </Left>
         </Header>
+        <InputGroup >
+          <FontAwesome name='search' style={{ paddingTop: 13}} size={15} color='#FF5E3A'/>
+          <Input
+            // style={styles.inputSearch} 
+            value={this.state.firstName}
+            placeholder='First Name'
+            onSubmitEditing={() => this.submit}
+            // returnKeyType="search"
+            />
+            <Input
+            // style={styles.inputSearch} 
+            value={this.state.lastName}
+            placeholder='Last Name'
+            onSubmitEditing={() => this.submit}
+            returnKeyType="search"
+            />
+             <Input
+            // style={styles.inputSearch} 
+            value={this.state.email}
+            name='location'
+            placeholder='Email'
+            onSubmitEditing={() => this.submit}
+            returnKeyType="search"
+            />
+        </InputGroup>
       </ScrollView>
     )
   }
